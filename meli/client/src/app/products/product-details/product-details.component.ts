9f4dc2b05e276c5544d37f7dfc81beec4334e049
gowesoft <gowesoft@gmail.com>
2020-12-10 15:53:16 -0300

    Last fixes

--- meli/client/src/app/products/product-details/product-details.component.ts
+++ meli/client/src/app/products/product-details/product-details.component.ts
@@ -4,21 +4,46 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from '../products.service';
 import { Item } from '../products.model';
 
+import { NumberFormatPipe } from '../../shared/number.pipe';
+
 @Component({
     selector: 'app-product-details',
     templateUrl: './product-details.component.html',
-    styleUrls: ['./product-details.component.scss']
+    styleUrls: ['./product-details.component.scss'],
+    providers: [NumberFormatPipe]
 })
 export class ProductDetailsComponent implements OnInit {
     id: string = '';
-    item!: Item;
+    item: Item = {
+        id: '',
+        address: {
+            state_name: ''
+        },
+        condition: '',
+        free_shipping: false,
+        title: '',
+        price: {
+            currency: '',
+            amount: 0,
+            decimals: ''
+        },
+        picture: '',
+        sold_quantity: 0,
+        description: ''
+
+    };
     author = {};
 
     constructor(
         private _Activatedroute: ActivatedRoute,
         private productService: ProductsService,
-        private router: Router
+        private router: Router,
+        private formatPipe: NumberFormatPipe
     ) { }
+
+    formatNumber(number: number): string {
+        return this.formatPipe.transform(number);
+    }
 
     getState(condition: string): any {
         if (condition === 'new') {
@@ -43,5 +68,4 @@ export class ProductDetailsComponent implements OnInit {
             });
         }
     }
-
 }
